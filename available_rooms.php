415fe207fdad433de938119e62ebf132379ce262
Jasmine Pillarisetti <jasminepill@gatech.edu>
2017-09-10 02:20:40 -0400

    Add files via upload

---
+++ available_rooms.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "medhacks";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT `Dept Name` AS dept,
+`Available Beds` AS avail,
+`Total Beds` AS capacity
+FROM `department`";
+
+$query = mysqli_query($conn, $sql);
+
+if (!$query) {
+	die ('SQL Error: ' . mysqli_error($conn));
+}
+?>
+<html>
+<head>
+	<title>Room Availability</title>
+  <link rel="stylesheet" href="patient_status_style.css">
+</head>
+<body>
+	<h1>Table 1</h1>
+	<table class="data-table">
+		<caption class="title">Department Status</caption>
+		<thead>
+			<tr>
+				<th>Department</th>
+				<th># Beds Available</th>
+        <th>Capacity</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		while ($row = mysqli_fetch_array($query))
+		{
+			echo '<tr>
+					<td>'.$row['dept'].'</td>
+          <td>'.$row['avail'].'</td>
+          <td>'.$row['capacity'].'</td>
+				</tr>';
+		}?>
+		</tbody>
+		</table>
+</body>
+<a href="available_rooms.php">
+  <img src="gps.png" alt="patient history" style="position: absolute; bottom:80px; left:650px;width:75px;height:75px;border:0;">
+</a>
+
+ <a href="trackER.html">
+  <img src="home.png" alt="homepage" style="position: absolute; bottom:80px; left:800px;width:75px;height:75px;border:0;">
+</a>
+</html>
---
+++ notifications.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "medhacks";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT CONCAT(`patient`.`First Initial`,' ',`patient`.`Last Name`) AS Name,
+`patient`.`Current Location` AS curr,
+`transfer_request`.`Proposed Change` AS next
+FROM `patient`,`transfer_request`
+WHERE `transfer_request`.`Patient ID` = `patient`.`Patient ID`";
+
+$query = mysqli_query($conn, $sql);
+
+if (!$query) {
+	die ('SQL Error: ' . mysqli_error($conn));
+}
+?>
+<html>
+<head>
+	<title>Pending Transfers</title>
+  <link rel="stylesheet" href="patient_status_style.css">
+</head>
+<body>
+	<h1>Table 1</h1>
+	<table class="data-table">
+		<caption class="title">Pending Patient Transfers</caption>
+		<thead>
+			<tr>
+				<th>Patient Name</th>
+				<th>Current Location</th>
+				<th>Next Location</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		while ($row = mysqli_fetch_array($query))
+		{
+			echo '<tr>
+					<td>'.$row['Name'].'</td>
+					<td>'.$row['curr'].'</td>
+          <td><div contenteditable>'.$row['next'].'</div></td>
+				</tr>';
+		}?>
+		</tbody>
+		</table>
+</body>
+<a href="available_rooms.php">
+  <img src="gps.png" alt="patient history" style="position: absolute; bottom:80px; left:650px;width:75px;height:75px;border:0;">
+</a>
+
+ <a href="trackER.html">
+  <img src="home.png" alt="homepage" style="position: absolute; bottom:80px; left:800px;width:75px;height:75px;border:0;">
+</a>
+</html>
